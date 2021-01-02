5003627aa11baf0894f22ccbbbf2e0a9c2364027
KangHyoengu <fkdla369@gmail.com>
2021-01-02 22:24:34 +0900

    2021-01-02 알고리즘 공부

---
+++ algorithm/src/chap01/Prac1.java
@@ -0,0 +1,74 @@
+package chap01;
+
+public class Prac1 {
+	
+	//4개의 값 중에서 최대값을 구하는메서드
+	static int max4(int a, int b, int c, int d) {
+		int max = a;
+		
+		if(b > max) {
+			max = b;
+		}
+		
+		if(c > max) {
+			max = c;
+		}
+		
+		if(d > max) {
+			max = d;
+		}
+		
+		return max;
+	}
+	
+	// 3개의 값 중 최소값을 구하는 메서드
+	static int min3(int a, int b, int c) {
+		int min = a;
+		
+		if(b < min) {
+			min = b;
+		}
+		
+		if(c < min) {
+			min = c;
+		}
+		
+		return min;
+	}
+	
+	// 4개의 값 중 최소값을 구하는 메서드
+	static int min4(int a, int b, int c, int d) {
+		int min = a;
+		
+		if(b < min) {
+			min = b;
+		}
+		
+		if(c < min) {
+			min = c;
+		}
+		
+		if(d < min) {
+			min = d;
+		}
+		
+		return min;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("max4(3, 2, 1, 4) = " + max4(3, 2, 1, 4));
+		System.out.println("max4(3, 2, 2, 3) = " + max4(3, 2, 2, 3));
+		System.out.println("max4(3, 4, 1, 2) = " + max4(3, 4, 1, 2));
+		System.out.println("max4(3, 2, 4, 3) = " + max4(3, 2, 4, 3));
+		System.out.println("min3(2, 1, 3) = " + min3(2, 1, 3));
+		System.out.println("min3(3, 3, 2) = " + min3(3, 3, 2));
+		System.out.println("min3(3, 3, 3) = " + min3(3, 3, 3));
+		System.out.println("min3(2, 2, 3) = " + min3(2, 2, 3));
+		System.out.println("min3(2, 3, 1) = " + min3(2, 3, 1));
+		System.out.println("min4(2, 4, 3, 2) = " + min4(2, 4, 3, 2));
+		System.out.println("min4(1, 3, 4, 2) = " + min4(1, 3, 4, 2));
+		System.out.println("min4(2, 3, 3, 4) = " + min4(2, 3, 3, 4));
+		System.out.println("min4(1, 2, 2, 3) = " + min4(1, 2, 2, 3));
+	}
+
+}
